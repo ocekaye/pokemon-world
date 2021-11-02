0a8b86187bbd4738cc5bf910fc33b4a4df0b9ed3
Okki Setyatmoko <styat_moko@rocketmail.com>
2021-11-02 20:55:42 +0700

    pokemon card hover effect

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,13 +1,19 @@
 import tw from "twin.macro";
 import styled from "@emotion/styled";
+
 const PokemonItemStyled = styled.div(tw`
-    flex
-    relative
-    rounded-xl
-    shadow-md
-    overflow-hidden
-    md:transition
-    md:duration-200
-    md:hover:shadow-2xl
-`);
-export default PokemonItemStyled;
+flex
+relative
+rounded-xl
+shadow-md
+overflow-hidden
+transition
+duration-300
+md:hover:shadow-2xl
+hover:rotate-0
+hover:z-10
+transform`);
+
+export default function Card(props) {
+  return <PokemonItemStyled>{props.children}</PokemonItemStyled>;
+}
