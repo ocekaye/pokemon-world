00b40d0dd27cf3b0bb2ca08c4ae47bf6915cd988
Okki Setyatmoko <styat_moko@rocketmail.com>
2021-11-05 21:27:32 +0700

    backpack page back button

--- pages/backpack.tsx
+++ pages/backpack.tsx
@@ -1,21 +1,37 @@
 import { useState, useEffect } from "react";
-
+import { useRouter } from "next/router";
 import Head from "next/head";
 import tw, { styled } from "twin.macro";
 import { MyPokemon, IPokemon } from "~/Db";
 import PokemonItem from "~/components/PokemonItem";
+import BackButton from "~/components/buttons/BackButton";
 const GridContainter = styled.div(
   tw`
-  grid grid-cols-2 
+  grid grid-cols-2
   gap-2
   px-2 pt-2
   sm:grid-cols-3 sm:gap-2 sm:px-3 sm:pt-3
   md:grid-cols-4 md:gap-3 md:px-4 md:pt-4
   lg:grid-cols-5 lg:gap-4
-  xl:grid-cols-6`
+  xl:grid-cols-6  
+  grid-flow-row-dense
+  `
 );
+
+const BackpackStyled = styled.div(tw`
+  flex
+`);
+
+const ButtonBack = styled.div(tw`
+  flex absolute left-4 bottom-4 transition-all hover:scale-125
+`);
 export default function Backpack() {
+  const router = useRouter();
   const [pokemons, setPokemons] = useState<IPokemon[]>([]);
+
+  const back = () => {
+    router.back();
+  };
   useEffect(() => {
     getPokemons();
   }, []);
@@ -26,12 +42,15 @@ export default function Backpack() {
   };
 
   return (
-    <div>
+    <BackpackStyled>
       <GridContainter>
         {pokemons.map((poke) => (
           <PokemonItem key={poke.pokeName} pokemonData={poke} />
         ))}
       </GridContainter>
-    </div>
+      <ButtonBack>
+        <BackButton onClick={back} />
+      </ButtonBack>
+    </BackpackStyled>
   );
 }
