54271d73ffdef49b3c1d1e072b46b61e7031dc6a
Okki Setyatmoko <styat_moko@rocketmail.com>
2021-11-06 03:07:43 +0700

    backpack background and header

--- pages/backpack.tsx
+++ pages/backpack.tsx
@@ -14,17 +14,34 @@ const GridContainter = styled.div(
   md:grid-cols-4 md:gap-3 md:px-4 md:pt-4
   lg:grid-cols-5 lg:gap-4
   xl:grid-cols-6  
-  grid-flow-row-dense
+  
   `
 );
 
 const BackpackStyled = styled.div(tw`
-  flex
+flex flex-col justify-center items-center pb-24 min-h-screen bg-gradient-to-b from-gray-500 to-gray-800
 `);
 
 const ButtonBack = styled.div(tw`
   flex absolute left-4 bottom-4 transition-all hover:scale-125
 `);
+
+const HeaderContainer = styled.div(tw`
+  fixed top-3 z-20 w-full
+`);
+
+const Header = styled.div(tw`
+  flex justify-around bg-gray-800 bg-opacity-90
+  rounded-full py-3 px-4 self-stretch
+  mx-4
+  md:mx-48
+  lg:mx-60
+`);
+
+const LocationText = styled.div(tw`
+  text-white text-lg font-bold
+`);
+
 export default function Backpack() {
   const router = useRouter();
   const [pokemons, setPokemons] = useState<IPokemon[]>([]);
@@ -43,6 +60,11 @@ export default function Backpack() {
 
   return (
     <BackpackStyled>
+      <HeaderContainer>
+        <Header>
+          <LocationText>My Backpack</LocationText>
+        </Header>
+      </HeaderContainer>
       <GridContainter>
         {pokemons.map((poke) => (
           <PokemonItem key={poke.pokeName} pokemonData={poke} />
